55e95f88f28fac48444bba30aa92aa82a258c0b2
Derek Bourgeois <derek@ibourgeois.com>
2015-02-03 11:29:15 -0500

    Create swapfile.sh

---
+++ apps/swapfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a Swapfile
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
